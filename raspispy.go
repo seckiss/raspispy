ba17f2893f11b248cc7f33981b88c60bd119b6f2
greg-sk <grzegorz.luczywo@securitykiss.com>
2016-09-15 17:46:31 +0200

    tcp server

--- raspispy.go
+++ raspispy.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
+	"net"
 	"os"
 	"runtime"
 	"time"
@@ -16,8 +17,47 @@ func main() {
 	r := bufio.NewReader(os.Stdin)
 	var ring = ringbuf.NewBuffer(5 * 1024 * 1024)
 	go ringWrite(ring, r)
-	time.Sleep(20 * time.Second)
-	ioutil.WriteFile("out.rasp", ring.Bytes(), 0644)
+	l, err := net.Listen("tcp", ":2222")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		conn.SetDeadline(time.Now().Add(30 * time.Second))
+		go func(c net.Conn) {
+			reader := ring.NewReader()
+			written, err := io.Copy(c, reader)
+			if err != nil {
+				fmt.Printf("io.Copy ended with error: %+v\n", err)
+			}
+			fmt.Printf("Written %d bytes\n", written)
+
+			/*			var b = make([]byte, 0, 4*1024)
+						for {
+							n, err := reader.Read(b[:cap(b)])
+							if err != nil {
+								break
+							}
+							if n == 0 {
+								continue
+							}
+							var b2 = make([]byte, 0, n)
+							conn.Write(b2)
+
+						}
+			*/
+			c.Close()
+
+		}(conn)
+
+	}
+
+	//time.Sleep(20 * time.Second)
+	//ioutil.WriteFile("out.rasp", ring.Bytes(), 0644)
 }
 
 func ringWrite(ring *ringbuf.Buffer, r io.Reader) {
